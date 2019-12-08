5cd6d3b6caed09ac1982ef22519eed0c377860e1
jculina <37914546+jculina@users.noreply.github.com>
2019-12-09 00:18:31 +0100

    Added NaslovnicaPage

--- gatsby-project/src/pages/index.js
+++ gatsby-project/src/pages/index.js
@@ -1,13 +1,87 @@
 import React from "react"
-import Layout from "../components/Layout/layout"
+import { graphql } from "gatsby"
 
-const NaslovnicaPage = () => {
+import Layout from "../components/Layout/layout"
+import "../styles/index.css"
+import Img from "gatsby-image"
+
+const NaslovnicaPage = props => {
   return (
     <Layout>
-      <h1>Naslovnica</h1>
-      <p>Ovo je Naslovna stranica.</p>
+      <main>
+        <p class="main-p-title">Najpopularnije</p>
+        <div id="main-container-najpopularnije">
+          <div class="main-container-item-najpopularnije">
+            <p class="main-item-najpopularnije-kategorija">Glazba</p>
+            <Img
+              className="main-najpopularnije-img"
+              fluid={props.data.glazba.childImageSharp.fluid}
+            />
+            <p class="main-item-najpopularnije-dogadaj">
+              Koncert Parnog valjka
+            </p>
+          </div>
+          <div class="main-container-item-najpopularnije">
+            <p class="main-item-najpopularnije-kategorija">Kazalište</p>
+            <Img
+              className="main-najpopularnije-img"
+              fluid={props.data.kazaliste.childImageSharp.fluid}
+            />
+            <p class="main-item-najpopularnije-dogadaj">Orašar</p>
+          </div>
+          <div class="main-container-item-najpopularnije">
+            <p class="main-item-najpopularnije-kategorija">Sport</p>
+            <Img
+              className="main-najpopularnije-img"
+              fluid={props.data.sport.childImageSharp.fluid}
+            />
+            <p class="main-item-najpopularnije-dogadaj">Hajduk-Dinamo</p>
+          </div>
+          <div class="main-container-item-najpopularnije">
+            <p class="main-item-najpopularnije-kategorija">Ostalo</p>
+            <Img
+              className="main-najpopularnije-img"
+              fluid={props.data.ostalo.childImageSharp.fluid}
+            />
+            <p class="main-item-najpopularnije-dogadaj">Kralj lavova</p>
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
 
 export default NaslovnicaPage
+
+export const query = graphql`
+  query {
+    glazba: file(relativePath: { eq: "naslovnica_glazba.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    kazaliste: file(relativePath: { eq: "naslovnica_kazaliste.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    sport: file(relativePath: { eq: "naslovnica_sport.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    ostalo: file(relativePath: { eq: "naslovnica_ostalo.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`

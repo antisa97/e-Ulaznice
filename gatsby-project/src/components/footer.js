ecf10de146d5bfe027bb20e293ee6fee2290fdcd
jculina <37914546+jculina@users.noreply.github.com>
2020-01-10 00:03:11 +0100

    Editing Footer

--- gatsby-project/src/components/footer.js
+++ gatsby-project/src/components/footer.js
@@ -1,10 +1,81 @@
 import React from "react"
 import FooterStyle from "../components/componentsStyles/footer.module.css"
+import youtube from "../images/youtube.png"
+import facebook from "../images/facebook.png"
+import instagram from "../images/instagram.png"
+import mail from "../images/mail.png"
+import phone from "../images/phone.png"
+import twitter from "../images/twitter.png"
 
 const Footer = () => {
   return (
     <footer className={FooterStyle.footer}>
-      <p>Napravljeno u sklopu kolegija Korisnička sučelja, © 2019</p>
+      <div className={FooterStyle.container}>
+        <div className={FooterStyle.findUs}>
+          <h5 className={FooterStyle.title}>
+            Potražite nas na društvenim mrežama
+          </h5>
+          <div className={FooterStyle.iconsContainer}>
+            <a href="https://www.facebook.com/" target="_blank">
+              <img className={FooterStyle.img} src={facebook} />
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+              <img className={FooterStyle.img} src={instagram} />
+            </a>
+            <a href="https://twitter.com/" target="_blank">
+              <img className={FooterStyle.img} src={twitter} />
+            </a>
+            <a href="https://www.youtube.com/" target="_blank">
+              <img className={FooterStyle.img} src={youtube} />
+            </a>
+          </div>
+        </div>
+        <div className={FooterStyle.links}>
+          <h5 className={FooterStyle.title}>Događanja u gradovima</h5>
+          <a
+            className={FooterStyle.link}
+            href="https://www.infozagreb.hr/dogadanja#ne-propustite"
+            target="_blank"
+          >
+            Zagreb
+          </a>
+          <a
+            className={FooterStyle.link}
+            href="https://www.dalmatia.hr/hr/kalendar-dogadanja"
+            target="_blank"
+          >
+            Split
+          </a>
+          <a
+            className={FooterStyle.link}
+            href="http://www.visitrijeka.hr/Sto_raditi/Kalendar_dogadanja"
+            target="_blank"
+          >
+            Rijeka
+          </a>
+          <a
+            className={FooterStyle.link}
+            href="https://www.tzosijek.hr/"
+            target="_blank"
+          >
+            Osijek
+          </a>
+        </div>
+        <div className={FooterStyle.contact}>
+          <h5 className={FooterStyle.title}>Kontakt</h5>
+          <div className={FooterStyle.number}>
+            <img className={FooterStyle.imgPhone} src={phone} />
+            <p className={FooterStyle.numberP}>(021) 147-578</p>
+          </div>
+          <div className={FooterStyle.mail}>
+            <img className={FooterStyle.imgMail} src={mail} />
+            <p className={FooterStyle.mailP}>e-ulaznice@ulaz.com</p>
+          </div>
+        </div>
+      </div>
+      <p className={FooterStyle.copyright}>
+        Napravljeno u sklopu kolegija Korisnička sučelja © 2019
+      </p>
     </footer>
   )
 }
--- gatsby-project/src/pages/dogadaji.js
+++ gatsby-project/src/pages/dogadaji.js
@@ -2,19 +2,85 @@ import React from "react"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
 import Layout from "../components/layout"
-import "../styles/dogadaji.css"
+import DogadajiStyle from "../styles/dogadaji.module.css"
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardDeck,
+  CardSubtitle,
+  CardBody,
+} from "reactstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 const DogadajiPage = props => {
   return (
     <Layout>
       <p className="dogadaji-title">Događaji</p>
-      <div className="search">
+      <CardDeck>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="/assets/256x186.svg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="/assets/256x186.svg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+        <Card>
+          <CardImg
+            top
+            width="100%"
+            src="/assets/256x186.svg"
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This card has even longer content
+              than the first to show that equal height action.
+            </CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
+      </CardDeck>
+      {/* <div className="search">
         <input
           type="search"
           className="search-box text"
           placeholder="Search..."
-        />
-        {/* <select className="select main-filter-box courses-input-row text">
+        /> */}
+      {/* <select className="select main-filter-box courses-input-row text">
           <option selected disabled>
             Pretraži prema...
           </option>
@@ -23,7 +89,7 @@ const DogadajiPage = props => {
           <option value="Mjesto">Mjesto</option>
           <option value="Cijena">Cijena</option>
         </select> */}
-      </div>
+      {/* </div>
       <div id="dogadaji-container">
         <div className="dogadaji-container-item">
           <Img
@@ -76,7 +142,7 @@ const DogadajiPage = props => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </Layout>
   )
 }

d4a04a2df8a3e5a6c46648daa2e0ffeb1c3f7247
jculina <37914546+jculina@users.noreply.github.com>
2020-01-14 01:15:37 +0100

    Changing DogadajiPage

--- gatsby-project/src/pages/dogadaji.js
+++ gatsby-project/src/pages/dogadaji.js
@@ -12,6 +12,17 @@ const DogadajiPage = props => {
       <div className={DogadajiStyle.page}>
         <div className={DogadajiStyle.pageDiv}>
           <h3 className={DogadajiStyle.pageTitle}>Događaji</h3>
+          <div id={DogadajiStyle.searchContainer}>
+            <Img
+              className={DogadajiStyle.searchImage}
+              fluid={props.data.searchIcon.childImageSharp.fluid}
+            />
+            <input
+              type="text"
+              id={DogadajiStyle.search}
+              placeholder="Pretraživanje"
+            />
+          </div>
           <div className={DogadajiStyle.cardContainer}>
             <div className={DogadajiStyle.card}>
               <div className={DogadajiStyle.cardImage}>
@@ -20,10 +31,82 @@ const DogadajiPage = props => {
                   fluid={props.data.glazba.childImageSharp.fluid}
                 />
               </div>
-              <p>koncert</p>
-              <Button outline color="danger">
-                Kupi
-              </Button>
+              <div className={DogadajiStyle.content}>
+                <p className={DogadajiStyle.cardTitle}>Parni valjak</p>
+                <div className={DogadajiStyle.category}>
+                  <p className={DogadajiStyle.categoryTitle}>Kultura</p>
+                  <Img
+                    className={DogadajiStyle.categoryImage}
+                    fluid={props.data.cultureIcon.childImageSharp.fluid}
+                  />
+                </div>
+                <div className={DogadajiStyle.dateLocation}>
+                  <p className={DogadajiStyle.location}>Pulska arena</p>
+                  <p className={DogadajiStyle.date}>17.01.2020. u 20:00</p>
+                </div>
+              </div>
+              <div className={DogadajiStyle.priceButton}>
+                <p className={DogadajiStyle.price}>80,00 kn</p>
+                <Button className={DogadajiStyle.button} outline color="danger">
+                  Kupi ulaznice
+                </Button>
+              </div>
+            </div>
+            <div className={DogadajiStyle.card}>
+              <div className={DogadajiStyle.cardImage}>
+                <Img
+                  className={DogadajiStyle.image}
+                  fluid={props.data.ostalo.childImageSharp.fluid}
+                />
+              </div>
+              <div className={DogadajiStyle.content}>
+                <p className={DogadajiStyle.cardTitle}>Kralj lavova</p>
+                <div className={DogadajiStyle.category}>
+                  <p className={DogadajiStyle.categoryTitle}>Film</p>
+                  <Img
+                    className={DogadajiStyle.categoryImage}
+                    fluid={props.data.filmIcon.childImageSharp.fluid}
+                  />
+                </div>
+                <div className={DogadajiStyle.dateLocation}>
+                  <p className={DogadajiStyle.location}>Cinestar Split</p>
+                  <p className={DogadajiStyle.date}>05.02.2020. u 17:00</p>
+                </div>
+              </div>
+              <div className={DogadajiStyle.priceButton}>
+                <p className={DogadajiStyle.price}>35,00 kn</p>
+                <Button className={DogadajiStyle.button} outline color="danger">
+                  Kupi ulaznice
+                </Button>
+              </div>
+            </div>
+            <div className={DogadajiStyle.card}>
+              <div className={DogadajiStyle.cardImage}>
+                <Img
+                  className={DogadajiStyle.image}
+                  fluid={props.data.sport.childImageSharp.fluid}
+                />
+              </div>
+              <div className={DogadajiStyle.content}>
+                <p className={DogadajiStyle.cardTitle}>Hajduk-Dinamo</p>
+                <div className={DogadajiStyle.category}>
+                  <p className={DogadajiStyle.categoryTitle}>Sport</p>
+                  <Img
+                    className={DogadajiStyle.categoryImage}
+                    fluid={props.data.sportIcon.childImageSharp.fluid}
+                  />
+                </div>
+                <div className={DogadajiStyle.dateLocation}>
+                  <p className={DogadajiStyle.location}>Poljud</p>
+                  <p className={DogadajiStyle.date}>22.01.2020. u 15:00</p>
+                </div>
+              </div>
+              <div className={DogadajiStyle.priceButton}>
+                <p className={DogadajiStyle.price}>100,00 kn</p>
+                <Button className={DogadajiStyle.button} outline color="danger">
+                  Kupi ulaznice
+                </Button>
+              </div>
             </div>
             <div className={DogadajiStyle.card}>
               <div className={DogadajiStyle.cardImage}>
@@ -32,25 +115,26 @@ const DogadajiPage = props => {
                   fluid={props.data.glazba.childImageSharp.fluid}
                 />
               </div>
-              <p>ERO S ONOGA SVIJETA</p>
-            </div>
-            <div className={DogadajiStyle.card}>
-              <div className={DogadajiStyle.cardImage}>
-                <Img
-                  className={DogadajiStyle.image}
-                  fluid={props.data.glazba.childImageSharp.fluid}
-                />
+              <div className={DogadajiStyle.content}>
+                <p className={DogadajiStyle.cardTitle}>Parni valjak</p>
+                <div className={DogadajiStyle.category}>
+                  <p className={DogadajiStyle.categoryTitle}>Kultura</p>
+                  <Img
+                    className={DogadajiStyle.categoryImage}
+                    fluid={props.data.cultureIcon.childImageSharp.fluid}
+                  />
+                </div>
+                <div className={DogadajiStyle.dateLocation}>
+                  <p className={DogadajiStyle.location}>Pulska arena</p>
+                  <p className={DogadajiStyle.date}>17.01.2020. u 20:00</p>
+                </div>
               </div>
-              <p>koncert</p>
-            </div>
-            <div className={DogadajiStyle.card}>
-              <div className={DogadajiStyle.cardImage}>
-                <Img
-                  className={DogadajiStyle.image}
-                  fluid={props.data.glazba.childImageSharp.fluid}
-                />
+              <div className={DogadajiStyle.priceButton}>
+                <p className={DogadajiStyle.price}>80,00 kn</p>
+                <Button className={DogadajiStyle.button} outline color="danger">
+                  Kupi ulaznice
+                </Button>
               </div>
-              <p>koncert</p>
             </div>
           </div>
         </div>
@@ -85,6 +169,34 @@ export const query = graphql`
       }
     }
     ostalo: file(relativePath: { eq: "naslovnica_ostalo.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    cultureIcon: file(relativePath: { eq: "culture.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    sportIcon: file(relativePath: { eq: "sport.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    filmIcon: file(relativePath: { eq: "film.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    searchIcon: file(relativePath: { eq: "searchModro.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG

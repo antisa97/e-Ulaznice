9dc05b3c69b319fc6dc9058572192165202cd0ef
jculina <37914546+jculina@users.noreply.github.com>
2020-01-20 10:44:49 +0100

    Changing header for mob

--- gatsby-project/src/components/headerBottom.js
+++ gatsby-project/src/components/headerBottom.js
@@ -13,26 +13,26 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import NavLink from "./links"
 import HeaderBottomStyle from "../components/componentsStyles/headerBottom.module.css"
 import home from "../images/iconHome.png"
-import events from "../images/iconCalendar2.png"
+import events from "../images/iconCalendar4.png"
 import news from "../images/iconNews.png"
 import blog from "../images/iconBlog.png"
 
 const Header = props => {
   return (
     <div className={HeaderBottomStyle.input} id={HeaderBottomStyle.inputBottom}>
-      <NavLink to="/" exact="true">
+      <NavLink to="/" exact="true" className={HeaderBottomStyle.iconContainer}>
         <img className={HeaderBottomStyle.icon} src={home}></img>
         <p className={HeaderBottomStyle.navbarName}>NASLOVNICA</p>
       </NavLink>
-      <NavLink to="/dogadaji">
+      <NavLink to="/dogadaji" className={HeaderBottomStyle.iconContainer}>
         <img className={HeaderBottomStyle.icon} src={events}></img>
         <p className={HeaderBottomStyle.navbarName}> DOGAĐAJI</p>
       </NavLink>
-      <NavLink to="/novosti">
+      <NavLink to="/novosti" className={HeaderBottomStyle.iconContainer}>
         <img className={HeaderBottomStyle.icon} src={news}></img>
         <p className={HeaderBottomStyle.navbarName}> NOVOSTI</p>
       </NavLink>
-      <NavLink to="/izvjestaji">
+      <NavLink to="/izvjestaji" className={HeaderBottomStyle.iconContainer}>
         <img className={HeaderBottomStyle.icon} src={blog}></img>
         <p className={HeaderBottomStyle.navbarName}> IZVJEŠTAJI</p>
       </NavLink>
